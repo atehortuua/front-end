8824c0c150d3b035dd0266ea305dbb136060b67a
atehortuua <matehortua32@gmail.com>
2025-11-04 18:57:12 -0500

    [+] se crean las rutas principales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Contacto } from './pages/contacto/contacto';
+import { Login } from './pages/login/login';
+import { Registro } from './pages/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component : Home},
+    {path: 'contacto', component : Contacto},
+    {path: 'login', component : Login},
+    {path: 'registro', component :  Registro},
+    {path: '**', redirectTo: ''} //redireccionar al inicio
+
+
+];
