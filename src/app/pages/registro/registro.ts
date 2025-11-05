bd2e89b87400e4f2f7ed750eb4d7d67fd8f4f9a2
atehortuua <matehortua32@gmail.com>
2025-11-04 19:37:24 -0500

    [+] formulario registro

--- src/app/pages/registro/registro.ts
+++ src/app/pages/registro/registro.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
 export class Registro {
+  user={
+    name : '',
+    lastName: '',
+    number: '',
+    email :'',
+    password : '',
+    password2: ''
 
+  }
+
+  register(){
+    console.log(' usurio creado')
+    console.log(this.user)
+  }
 }
