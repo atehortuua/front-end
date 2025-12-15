52093712420ea4b5848955626d6e78bccfe058d3
atehortuua <matehortua32@gmail.com>
2025-12-15 14:10:51 -0500

    [+] se ajusta diseño de Home, Login, Register

--- src/app/dashboard/products/update-product/update-product.ts
+++ src/app/dashboard/products/update-product/update-product.ts
@@ -1,16 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { Product } from '../../../services/product';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-product',
-  imports: [FormsModule],
+  imports: [  RouterLink,FormsModule],
   templateUrl: './update-product.html',
   styleUrl: './update-product.css',
 })
 export class UpdateProduct {
   private productService = inject (Product);
+  private router = inject (Router)
   private route = inject(ActivatedRoute)
   id: any = ''
 
@@ -36,6 +37,7 @@ ngOnInit(){
       next: (res: any)=> {
         console.log(res);
       alert('producto Actualizado')
+      this.router.navigate(['/updateProduct/:id'])
       },
       error:(err)=>{
         console.log(err)
@@ -47,7 +49,10 @@ ngOnInit(){
     this.productService.getProductsById(id).subscribe({
       next: (res : any)=>{
         console.log(res);
+        
         this.product = res.product
+        
+        
       },
       error : (err) =>{
         console.log(err)
